d8c40b50a00c2448d9f31ae50e0c2643b77f235e
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2021-09-28 13:38:01 +0200

    add main library test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import com.company.library.Book;
+import com.company.library.Library;
+
 public class Main {
 
     public static void main(String[] args) {
+        Library library = new Library();
 
+        Book book = new Book("The Hobbit");
+
+        if (library.lend(book)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Not available");
+        }
+
+        if (library.lend(book)) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Not available");
+        }
     }
 }
